237be66a0324e46797c4b237ad9784a4bf18bc23
mattn <mattn.jp@gmail.com>
2011-12-16 17:55:36 +0900

    remove update.bat. use update.pl

--- update.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-mv -f coffee.cc coffee.cc.orig
-cat coffee.cc.orig | sed -n "1,/^\/\/ DO NOT EDIT/p" > coffee.cc
-curl -s http://jashkenas.github.com/coffee-script/extras/coffee-script.js | sed "s/\([\\""""]\)/\\\1/g" | sed "s/^/""""/g" | sed "s/$/""""/g" >> coffee.cc
-printf ;>> coffee.cc
---
+++ update.pl
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+use LWP::Simple;
+use File::Slurp;
+
+my $lines = read_file('coffee.cc', { binmode => ':raw' });
+$lines = substr($lines, 0, index($lines, "// DO NOT EDIT\n")+15);
+
+my $res = get('http://jashkenas.github.com/coffee-script/extras/coffee-script.js');
+$lines .= join("\n", map { s/([\\"])/\\$1/g; "\"$_\"" } split(/\n/, $res)) . ";\n";
+
+rename 'coffee.cc', 'coffee.cc.orig';
+write_file 'coffee.cc', {binmode => ':raw'}, $lines;
