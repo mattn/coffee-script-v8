bf7af4854c5198c839537e7a83e8e56845f2b42c
mattn <mattn.jp@gmail.com>
2011-12-20 16:09:25 +0900

    upgrade to coffeescript v1.2

--- update.pl
+++ update.pl
@@ -6,7 +6,7 @@ use File::Slurp;
 my $lines = read_file('coffee.cc', { binmode => ':raw' });
 $lines = substr($lines, 0, index($lines, "// DO NOT EDIT\n")+15);
 
-my $res = get('http://jashkenas.github.com/coffee-script/extras/coffee-script.js');
+my $res = get('http://coffeescript.org/extras/coffee-script.js');
 $lines .= join("\n", map { s/([\\"])/\\$1/g; "\"$_\"" } split(/\n/, $res)) . ";\n";
 
 rename 'coffee.cc', 'coffee.cc.orig';
